4f929914fc9157d33c7f34723e4d9140e403f626
mohancsgithub <84985948+mohancsgithub@users.noreply.github.com>
2021-05-31 22:20:12 +0530

    Add files via upload

---
+++ MyFile1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class MyDetails
+    {
+        public string FirstName { get; set; }        
+        public string DOB { get; set; }    
+        public string Location { get; set; }
+    }
+}
